2511860d9b643bf27077d29fda60af9bd69c3e6b
Aleksandra Jemielity <ajemielity@o2.pl>
2017-11-29 17:20:12 +0100

    add formspree.io to form, add bootstrap gallery

---
+++ sendForm.js
@@ -0,0 +1,34 @@
+/*
+/!*
+function sendFrom() {
+  document.getElementsByClassName('contact__form').addEventListener('submit', function (event) {
+    event.preventDefault()
+    event.stopPropagation()
+    this.reset()
+}
+*!/
+
+sendFrom = () => {
+  document.getElementsByClassName('contact__form').addEventListener('submit', fzmiennazwe = (event) => {
+    event.preventDefault()
+    event.stopPropagation()
+    this.reset()
+  }
+
+}
+
+
+
+
+
+
+/!**********************************************************************
+ * Show message on form submit. One can run the game from that place  *
+ **********************************************************************!/
+
+document.getElementsByClassName('contact__form').addEventListener('submit', function (event) {
+  event.preventDefault()
+  event.stopPropagation()
+  this.reset()
+}
+*/
